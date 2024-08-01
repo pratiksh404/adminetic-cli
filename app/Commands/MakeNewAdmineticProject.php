527bf8ab4b1c7d46f965724b53a8ba9d0e69a480
pratiksh404 <pratikdai404@gmail.com>
2024-08-01 20:00:20 +0545

    install laravel 10 only

--- app/Commands/MakeNewAdmineticProject.php
+++ app/Commands/MakeNewAdmineticProject.php
@@ -42,7 +42,7 @@ class MakeNewAdmineticProject extends Command
         $this->info("Creating new adminetic project: $name ...");
         /* Downloading Laravel */
         $this->task('Installing Laravel', function () use ($name) {
-            $process = shell_exec("laravel new $name");
+            $process = shell_exec("composer create-project laravel/laravel:^10.* $name");
 
             return !empty($process);
         });
